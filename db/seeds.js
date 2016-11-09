d1fe8783386000a6fcf115992e3d7a221c08e660
Barrett Cope <barrettcope@gmail.com>
2016-11-09 13:25:30 -0500

    Seeds working but not referencing properly

--- db/seeds.js
+++ db/seeds.js
@@ -1,8 +1,8 @@
 var db = require("./connection.js")
 var Schema = require("./schema.js")
 
-var Gallery = db.models.Gallery
-var Photo = db.models.Photo
+var Gallery = Schema.Gallery
+var Photo = Schema.Photo
 
 Gallery.remove({}).then( _ => {
   console.log("remove galleries completed");
@@ -19,46 +19,46 @@ Gallery.remove({}).then( _ => {
       console.log("Error: ",err)
     } else {
       console.log("Success: ",object)
-    }})
+    }}).then( _ => {
+      Photo.remove({}).then( _ => {
+        console.log("remove photos completed");
+        var Photo1 = new Photo({
+          name: "Cat in the Hat",
+          description: "Mike Rubin Halloween costume in 2004",
+          photoUrl: "http://www.pbase.com/mrubin/image/35627192/medium.jpg",
+          gallery: [MikesGallery]
+        })
+        var Photo2 = new Photo({
+          name: "Mario",
+          description: "Mike Rubin Halloween costume in 2006",
+          photoUrl: "http://www.pbase.com/mrubin/image/70300815/medium.jpg",
+          gallery: [MikesGallery,BarrettsGallery]
+        })
+        var Photo3 = new Photo({
+          name: "Pizza",
+          description: "Dinner last night",
+          photoUrl: "https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/13658884_540298029495912_110832229_n.jpg?ig_cache_key=MTMyMzU0MjI3MDI1OTMyNzEzMg%3D%3D.2",
+          gallery: [BarrettsGallery]
+        })
+        Photo1.save( (err, object) => {
+          if (err) {
+            console.log("Error: ",err)
+          } else {
+            console.log("Success: ",object)
+          }})
+        Photo2.save( (err, object) => {
+          if (err) {
+            console.log("Error: ",err)
+          } else {
+            console.log("Success: ",object)
+          }})
+        Photo3.save( (err, object) => {
+          if (err) {
+            console.log("Error: ",err)
+          } else {
+            console.log("Success: ",object)
+            process.exit()
+          }})
+      })
+    })
 })
-Photo.remove({}).then( _ => {
-  console.log("remove photos completed");
-  var Photo1 = new Photo({
-    name: "Cat in the Hat",
-    description: "Mike Rubin Halloween costume in 2004",
-    photoUrl: "http://www.pbase.com/mrubin/image/35627192/medium.jpg",
-    gallery: [MikesGallery]
-  })
-  var Photo2 = new Photo({
-    name: "Mario",
-    description: "Mike Rubin Halloween costume in 2006",
-    photoUrl: "http://www.pbase.com/mrubin/image/70300815/medium.jpg",
-    gallery: [MikesGallery,BarrettsGallery]
-  })
-  var Photo3 = new Photo({
-    name: "Pizza",
-    description: "Dinner last night",
-    photoUrl: "https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/13658884_540298029495912_110832229_n.jpg?ig_cache_key=MTMyMzU0MjI3MDI1OTMyNzEzMg%3D%3D.2",
-    gallery: [BarrettsGallery]
-  })
-  Photo1.save( (err, object) => {
-    if (err) {
-      console.log("Error: ",err)
-    } else {
-      console.log("Success: ",object)
-    }})
-  Photo2.save( (err, object) => {
-    if (err) {
-      console.log("Error: ",err)
-    } else {
-      console.log("Success: ",object)
-    }})
-  Photo3.save( (err, object) => {
-    if (err) {
-      console.log("Error: ",err)
-    } else {
-      console.log("Success: ",object)
-    }})
-})
-
-process.exit()
