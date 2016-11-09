0540c58fca02d432d7c3ca2df44ed018df75bd23
Michael Rubin <michael@rubinsworld.com>
2016-11-09 13:49:09 -0500

    index root working

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 /* our project Smile
 Smile captures your photos and allows you to add title, and description for later gouping with a gallery
  */
- 
+
+ var express = require("express");
+ var db = require("./db/connection");
+ var schema=require("./db/schema");
+
+ var app = express();
+
+ var Gallery = schema.Gallery
+ var Photo =  schema.Photo
+
+
+ app.set("port", process.env.PORT || 3000);
+ app.use("/assets", express.static("public"));
+
+app.get("/",function (req,res){
+  console.log("Root route ")
+  Photo.find({}).then ( photos => {
+      res.json(photos);
+  })
+})
+
+
+
+ app.listen(app.get("port"),function(){
+     console.log("Smile for your photos!")
+ })
